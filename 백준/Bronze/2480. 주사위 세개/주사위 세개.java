22c979729ce35704d8e2e4e15d85a020f22cfcbf
hagyeom <younha3710@naver.com>
2025-01-20 00:25:02 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+
+		if (a == b && b == c) {
+			System.out.println(10000 + a * 1000);
+		} else if ((a == b && b != c) || (a == c && b != c)) {
+			System.out.println(1000 + a * 100);
+		} else if (b == c && a != b) {
+			System.out.println(1000 + b * 100);
+		} else if (a != b && b != c) {
+			System.out.println(Math.max(a, Math.max(b, c)) * 100);
+		}
+
+		scanner.close();
+	}
+
+}
