e51777f3c2e90315e2155d558bc8d406cc94fc2a
hagyeom <younha3710@naver.com>
2025-01-23 14:56:04 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 2.70 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,49 +1,19 @@
+import java.util.*;
+
 class Solution {
-    // 주어진 배열에서 divisor로 나누어 떨어지는 요소를 찾는 메서드
-    public int[]findDivisionNumbers(int[]arr,int divisor){
-        int count=0;
-        for(int num:arr){
-            if(num%divisor==0){
-                count++;
+    public ArrayList<Integer> solution(int[] arr, int divisor) {
+        ArrayList<Integer>answer=new ArrayList<>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]%divisor==0){
+                answer.add(arr[i]);
             }
         }
-        
-        if(count==0){
-            return new int[]{-1};
+        if(answer.size()==0){
+            answer.add(-1);
         }
         
-        int[]divisibleNumbers=new int[count];
-        int index=0;
-        for(int num:arr){
-            if(num%divisor==0){
-                divisibleNumbers[index++]=num;
-            }
-        }
-        
-        return divisibleNumbers;
-    }
-    
-    // 배열을 오름차순으로 정렬하는 메서드
-    public void sortArray(int[]array){
-        for(int i=0;i<array.length-1;i++){
-            for(int j=i+1;j<array.length;j++){
-                if(array[i]>array[j]){
-                    int temp=array[i];
-                    array[i]=array[j];
-                    array[j]=temp;
-                }
-            }
-        }
-    }
-    
-    public int[] solution(int[] arr, int divisor) {
-        int[] answer = findDivisionNumbers(arr,divisor);
-        
-        if(answer.length==1&&answer[0]==-1){
-            return answer;
-        }
-        
-        sortArray(answer);
+        answer.sort(Comparator.naturalOrder());
         
         return answer;
     }
