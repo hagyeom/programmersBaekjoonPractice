0bd7bdb57254860c318d9b10e97fe70572100f55
hagyeom <younha3710@naver.com>
2025-02-10 15:04:18 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.18 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,8 @@
+// String의 인스턴스 메서드 split()
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        answer=my_string.split(" ");
+        return answer;
+    }
+}
