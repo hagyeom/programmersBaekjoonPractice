f47d53c384e0e1bdb0f35e7f60e9256b2b82a256
hagyeom <younha3710@naver.com>
2025-02-27 15:19:54 +0900

    [Bronze III] Title: 알람 시계, Time: 196 ms, Memory: 18516 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int h = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        if (m >= 45) {
+            m -= 45;
+        } else {
+            m += 15;
+            if (h == 0) {
+                h = 23;
+            } else {
+                h--;
+            }
+        }
+
+        System.out.println(h + " " + m);
+
+        scanner.close();
+    }
+}
