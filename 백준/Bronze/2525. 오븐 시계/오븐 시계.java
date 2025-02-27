7a7855c411659a83b4ff720f204706e0b7995cf4
hagyeom <younha3710@naver.com>
2025-02-27 16:12:59 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18516 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int a = scanner.nextInt(); // 현재 시각(시)
-        int b = scanner.nextInt(); // 현재 시각(분)
-        int c = scanner.nextInt(); // 요리하는 데 필요한 시간
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-        int endH = (a + (b + c) / 60) % 24; // 종료 시각(시)
-        int endM = (b + c) % 60; // 종료 시각(분)
+        int endH = (a + (b + c) / 60) % 24;
+        int endM = (b + c) % 60;
 
         System.out.println(endH + " " + endM);
+
         scanner.close();
     }
 }
