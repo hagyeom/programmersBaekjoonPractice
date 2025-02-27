e8bf438e499c0d381e073fe38ea73fa4b44bafff
hagyeom <younha3710@naver.com>
2025-02-27 13:16:07 +0900

    [Bronze V] Title: 윤년, Time: 172 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,19 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
 
-		int year = scanner.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
 
-		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-			System.out.println("1");
-		} else {
-			System.out.println("0");
-		}
-
-		scanner.close();
-	}
-
+        scanner.close();
+    }
 }
