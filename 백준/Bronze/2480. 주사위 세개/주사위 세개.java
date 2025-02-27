c4502afc27b5ca3058901420da0cc1d6c8e21616
hagyeom <younha3710@naver.com>
2025-02-27 16:21:09 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int c = scanner.nextInt();
+        int prizeMoney = 0;
+        int maxNum = 0;
 
-		if (a == b && b == c) {
-			System.out.println(10000 + a * 1000);
-		} else if ((a == b && b != c) || (a == c && b != c)) {
-			System.out.println(1000 + a * 100);
-		} else if (b == c && a != b) {
-			System.out.println(1000 + b * 100);
-		} else if (a != b && b != c) {
-			System.out.println(Math.max(a, Math.max(b, c)) * 100);
-		}
+        if (a == b && b == c) {
+            prizeMoney = 10000 + a * 1000;
+        } else if ((a == b && b != c) || (a == c && a != b)) {
+            prizeMoney = 1000 + a * 100;
+        } else if (b == c && a != b) {
+            prizeMoney = 1000 + b * 100;
+        } else {
+            maxNum = Math.max(Math.max(a, b), c);
+            prizeMoney = maxNum * 100;
+        }
 
-		scanner.close();
-	}
+        System.out.println(prizeMoney);
 
+        scanner.close();
+    }
 }
