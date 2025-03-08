3126895d414c1f31dfd36a9f622d0c00cc789f00
hagyeom <younha3710@naver.com>
2025-03-08 19:48:40 +0900

    [Bronze I] Title: 평균, Time: 212 ms, Memory: 19048 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int max = 0;
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            sum += ((double) arr[i] / max) * 100;
+        }
+        System.out.println(sum / n);
+
+        sc.close();
+    }
+}
