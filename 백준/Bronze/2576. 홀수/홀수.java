d1c24a22827e026f091e4573ad55287f5d0aba94
hagyeom <younha3710@naver.com>
2025-03-13 23:24:18 +0900

    [Bronze III] Title: 홀수, Time: 168 ms, Memory: 17656 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.java
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -1,40 +1,33 @@
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> input = new ArrayList<>();
+        ArrayList<Integer> odd = new ArrayList<>();
 
-		Vector<Integer> v = new Vector<Integer>(7);
-		Vector<Integer> odd = new Vector<Integer>();
+        for (int i = 0; i < 7; i++) {
+            int num = scanner.nextInt();
+            input.add(num);
+            if (num % 2 != 0) {
+                odd.add(num);
+            }
+        }
+        if (odd.isEmpty()) {
+            System.out.println(-1);
+        } else {
+            int sum = 0;
+            for (int oddNums : odd) {
+                sum += oddNums;
+            }
+            int min = Collections.min(odd);
+            System.out.println(sum);
+            System.out.println(min);
+        }
 
-		for (int i = 0; i < 7; i++) {
-			v.add(scanner.nextInt());
-		}
-
-		for (int i = 0; i < 7; i++) {
-			if (v.get(i) % 2 != 0) {
-				odd.add(v.get(i));
-			}
-		}
-
-		if (odd.isEmpty()) {
-			System.out.println("-1");
-		} else {
-			int sum = 0;
-			for (int i = 0; i < odd.size(); i++) {
-				Collections.sort(odd);
-				int n = odd.elementAt(i);
-				sum += n;
-			}
-			int min = Math.min(odd.firstElement(), odd.lastElement());
-			System.out.println(sum);
-			System.out.println(min);
-		}
-
-		scanner.close();
-	}
-
+        scanner.close();
+    }
 }
