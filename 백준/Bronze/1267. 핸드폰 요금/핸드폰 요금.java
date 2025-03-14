a5ffcae5501c46328dab6ea9e6aeb9b14826d501
hagyeom <younha3710@naver.com>
2025-03-14 16:07:35 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 184 ms, Memory: 18256 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.java
@@ -1,26 +1,22 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[] callTimes = new int[N];
-        for (int i = 0; i < N; ++i) {
-            callTimes[i] = scanner.nextInt();
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt(); // 통화 횟수
+        int y=0, m=0; // 영식 요금, 민식 요금
+        for(int i=0;i<n;i++){
+            int t=sc.nextInt(); // 통화 시간
+            y+=((t/30)+1)*10; // 영식 요금
+            m+=((t/60)+1)*15; // 민식 요금
         }
-
-        int yCost = 0, mCost = 0;
-        for (int i = 0; i < N; ++i) {
-            yCost += (callTimes[i] / 30 + 1) * 10;
-            mCost += (callTimes[i] / 60 + 1) * 15;
+        if(y<m){ // 영식 요금이 더 싸면
+            System.out.println("Y "+y);
+        }else if(y>m){ // 민식 요금이 더 싸면
+            System.out.println("M "+m);
+        }else{ // 같으면
+            System.out.println("Y M "+y);
         }
-
-        if (yCost < mCost) {
-            System.out.println("Y " + yCost);
-        } else if (yCost > mCost) {
-            System.out.println("M " + mCost);
-        } else {
-            System.out.println("Y M " + yCost);
-        }
+        sc.close();
     }
 }
