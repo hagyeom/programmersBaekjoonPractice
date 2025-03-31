fb74496cf120780ef2d54d90849afa32466c8697
hagyeom <younha3710@naver.com>
2025-03-31 11:17:30 +0900

    [level 0] Title: 배열 자르기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,5 +1,5 @@
 function solution(numbers, num1, num2) {
-    var answer = [];
-    answer=numbers.splice(num1,num2-num1+1);
+    var answer = numbers.slice(num1, num2 + 1);
+    
     return answer;
 }
