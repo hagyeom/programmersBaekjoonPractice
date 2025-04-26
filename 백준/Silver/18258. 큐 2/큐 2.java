4e1f55328718a5a342d32a5beba248448069f7df
hagyeom <younha3710@naver.com>
2025-04-26 13:45:27 +0900

    [Silver IV] Title: 큐 2, Time: 1376 ms, Memory: 348424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    int x = Integer.parseInt(st.nextToken());
+                    queue.add(x);
+                    break;
+                case "pop":
+                    bw.write((queue.isEmpty() ? "-1" : String.valueOf(queue.poll())) + "\n");
+                    break;
+                case "size":
+                    bw.write(String.valueOf(queue.size()) + "\n");
+                    break;
+                case "empty":
+                    bw.write((queue.isEmpty() ? "1" : "0") + "\n");
+                    break;
+                case "front":
+                    bw.write((queue.isEmpty() ? "-1" : String.valueOf(queue.peek())) + "\n");
+                    break;
+                case "back":
+                    bw.write((queue.isEmpty() ? "-1" : String.valueOf(((LinkedList<Integer>) queue).getLast())) + "\n");
+                    break;
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
