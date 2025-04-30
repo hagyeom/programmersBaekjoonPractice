f320d1a9d0c43d38b0e8e0b75f31dd624588071e
hagyeom <younha3710@naver.com>
2025-04-30 20:49:23 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,21 +1,34 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        // 문자 배열 준비
         int[] arr = new int[26];
+
         for (int i = 0; i < 26; i++) {
-            arr[i] = -1; // -1로 초기화
+            arr[i] = -1;
         }
+
+        // 문자열 입력 받기
+        String s = br.readLine();
+
+        // 문자 위치 탐색
         for (int i = 0; i < s.length(); i++) {
-            if (arr[s.charAt(i) - 'a'] == -1) { // 처음 등장한 문자라면
-                arr[s.charAt(i) - 'a'] = i; // 해당 문자의 위치를 저장
+            int idx = s.charAt(i) - 'a'; // 알파벳의 인덱스를 계산
+            if (arr[idx] == -1) { // 해당 알파벳의 초기 값이 -1인 경우,
+                arr[idx] = i; // 현재 인덱스를 배열에 저장
             }
         }
+
+        // 결과 출력
         for (int i = 0; i < 26; i++) {
-            System.out.print(arr[i] + " ");
+            bw.write(arr[i] + " ");
         }
-        sc.close();
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
