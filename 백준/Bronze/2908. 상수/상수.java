7f7ba5194e0beb6a18e0793e19a4d4a57edf8ef1
hagyeom <younha3710@naver.com>
2025-05-03 00:58:56 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,11 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        String[] input = sc.nextLine().split(" ");
-        int a = Integer.parseInt(new StringBuilder(input[0]).reverse().toString()); // reverse() 는 StringBuilder 객체의
-                                                                                    // 문자열을 뒤집는 메소드
-        int b = Integer.parseInt(new StringBuilder(input[1]).reverse().toString());
-        System.out.println(a > b ? a : b);
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String aStr = st.nextToken();
+        String bStr = st.nextToken();
+
+        String reversedAStr = new StringBuilder(aStr).reverse().toString();
+        String reversedBStr = new StringBuilder(bStr).reverse().toString();
+
+        int reversedA = Integer.parseInt(reversedAStr);
+        int reversedB = Integer.parseInt(reversedBStr);
+
+        bw.write(reversedA > reversedB ? reversedAStr : reversedBStr);
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
