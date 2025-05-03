1512907862d02bcddbcca2e4ce215257bc8df412
hagyeom <younha3710@naver.com>
2025-05-03 13:48:17 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,33 +1,45 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new java.io.OutputStreamWriter(System.out));
 
-        String s = sc.next(); // 입력받은 문자열
-        int sum = 0; // 문자열을 다이얼로 변환한 값
-
+        String s = br.readLine();
+        int totalTime = 0;
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i); // 문자열을 문자로 변환
-            if (c == 'A' || c == 'B' || c == 'C') {
-                sum += 3; // A,B,C는 3초
-            } else if (c == 'D' || c == 'E' || c == 'F') {
-                sum += 4; // D,E,F는 4초
-            } else if (c == 'G' || c == 'H' || c == 'I') {
-                sum += 5; // G,H,I는 5초
-            } else if (c == 'J' || c == 'K' || c == 'L') {
-                sum += 6; // J,K,L는 6초
-            } else if (c == 'M' || c == 'N' || c == 'O') {
-                sum += 7; // M,N,O는 7초
-            } else if (c == 'P' || c == 'Q' || c == 'R' || c == 'S') {
-                sum += 8; // P,Q,R,S는 8초
-            } else if (c == 'T' || c == 'U' || c == 'V') {
-                sum += 9; // T,U,V는 9초
-            } else if (c == 'W' || c == 'X' || c == 'Y' || c == 'Z') {
-                sum += 10; // W,X,Y,Z는 10초
+            switch (s.charAt(i)) {
+                case 'A', 'B', 'C':
+                    totalTime += 3;
+                    break;
+                case 'D', 'E', 'F':
+                    totalTime += 4;
+                    break;
+                case 'G', 'H', 'I':
+                    totalTime += 5;
+                    break;
+                case 'J', 'K', 'L':
+                    totalTime += 6;
+                    break;
+                case 'M', 'N', 'O':
+                    totalTime += 7;
+                    break;
+                case 'P', 'Q', 'R', 'S':
+                    totalTime += 8;
+                    break;
+                case 'T', 'U', 'V':
+                    totalTime += 9;
+                    break;
+                case 'W', 'X', 'Y', 'Z':
+                    totalTime += 10;
+                    break;
             }
         }
-        System.out.println(sum);
-        sc.close();
+        bw.write(totalTime + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
