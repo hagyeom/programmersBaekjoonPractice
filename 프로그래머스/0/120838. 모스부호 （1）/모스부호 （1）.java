9741650aa2691b9b84e552b293eea8a2ddd47c9f
hagyeom <younha3710@naver.com>
2025-05-19 13:07:07 +0900

    [level 0] Title: 모스부호 (1), Time: 0.25 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,17 +1,20 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String letter) {
-        String[] morseCodes=letter.split(" ");
-        HashMap<String,String>morseMap=new HashMap<>();
-        
-        morseMap.put(".-", "a"); morseMap.put("-...", "b"); morseMap.put("-.-.", "c"); morseMap.put("-..", "d"); morseMap.put(".", "e"); morseMap.put("..-.", "f"); morseMap.put("--.", "g"); morseMap.put("....", "h"); morseMap.put("..", "i"); morseMap.put(".---", "j"); morseMap.put("-.-", "k"); morseMap.put(".-..", "l"); morseMap.put("--", "m"); morseMap.put("-.", "n"); morseMap.put("---", "o"); morseMap.put(".--.", "p"); morseMap.put("--.-", "q"); morseMap.put(".-.", "r"); morseMap.put("...", "s"); morseMap.put("-", "t"); morseMap.put("..-", "u"); morseMap.put("...-", "v"); morseMap.put(".--", "w"); morseMap.put("-..-", "x"); morseMap.put("-.--", "y"); morseMap.put("--..", "z");
-        
-        StringBuilder decodedMessage=new StringBuilder();
-        for(String code:morseCodes){
-            decodedMessage.append(morseMap.get(code));
+        String answer = "";
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        Map<String, String> morseMap = new HashMap<String, String>();
+        for (int i = 0; i < morse.length; i++) {
+            char c = (char) ('a' + i);
+            morseMap.put(morse[i], String.valueOf(c));
+        }   
+        String[] words = letter.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < words.length; i++) {
+            sb.append(morseMap.get(words[i]));
         }
-        
-        return decodedMessage.toString();
+        answer = sb.toString();
+        return answer;
     }
 }
